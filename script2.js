5873464b54db1266e44ba81e1066c66476a04bdf
Ava-creates <avanitiwari2002@gmail.com>
2022-05-03 17:43:23 +0400

    to-do and some other updates

---
+++ script2.js
@@ -0,0 +1,12 @@
+document.getElementById("match").onclick = function () {
+    location.href = "match.html";
+}
+
+document.getElementById("to_do").onclick = function () {
+    location.href = "to_do.html";
+}
+
+document.getElementById("help").onclick = function () {
+    location.href = "help.html";
+}
+
