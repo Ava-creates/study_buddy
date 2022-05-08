40b41793dfd4434f59678cce7d2a167e3810bd0b
dweggy <colacolaojm@hotmail.com>
2022-05-08 20:57:31 +0400

    base for fetch api calls

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("loginButton").onclick = async function () {
+  // location.href = "match.html";
+
+  
+  // fetch('https://studybuddy-server.herokuapp.com/google').then(async function (response) {
+  //   // The API call was successful!
+  //   let json = await response.json();
+  //   console.log(json);
+  // }).catch(function (err) {
+  //   // There was an error
+  //   console.warn('Something went wrong.', err);
+  // });
+}
